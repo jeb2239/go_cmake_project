5e2dc1689eb6aab9f85021c7d4939dc07fa62aa0
Jonathan Barrios <jeb2239@columbia.edu>
2017-08-26 14:37:07 -0400

    my files

---
+++ src/nova/apps/examples/CMakeFiles/loggerExample.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loggerExample"
+  "/.timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/loggerExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
